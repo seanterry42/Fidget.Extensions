9a955a4fc452aa0364d7ea4df5d8d3c635a44e0f
Sean Terry <seanterry@outlook.com>
2017-06-07 15:14:24 -0500

    Name-based GUIDs

---
+++ src/Fidget.Extensions.Guids/src/NamedGuidAlgorithm.cs
@@ -0,0 +1,21 @@
+﻿namespace Fidget.Extensions.Guids
+{
+    /// <summary>
+    /// Enumeration of named GUID algorithms.
+    /// </summary>
+
+    public enum NamedGuidAlgorithm
+    {
+        /// <summary>
+        /// Generates a name-based GUID using the MD5 algorithm.
+        /// </summary>
+        
+        MD5 = 3,
+
+        /// <summary>
+        /// Generates a name-based GUID using the SHA-1 algorithm (preferred).
+        /// </summary>
+        
+        SHA1 = 5,
+    }
+}
