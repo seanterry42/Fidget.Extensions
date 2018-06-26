88581a5f36343bd5af97e94c31d9928e4f3b1bc5
Sean Terry <seanterry@outlook.com>
2018-06-26 13:19:51 -0500

    Add Compute method for Named guids
    This accounts for bugs in Create method

--- src/Factories/NamedGuidFactory.cs
+++ src/Factories/NamedGuidFactory.cs
@@ -54,12 +54,13 @@ namespace Identifiable.Factories
         };
 
         /// <summary>
-        /// Creates and return a name-based GUID using the given algorithm as defined in https://tools.ietf.org/html/rfc4122#section-4.3.
+        /// Creates and return a name-based GUID that is not compatible with RFC 4122.
         /// </summary>
         /// <param name="algorithm">Hash algorithm to use for generating the name. SHA-1 is recommended.</param>
         /// <param name="namespace">Name space identifier.</param>
         /// <param name="name">Name for which to create a GUID.</param>
         
+        [Obsolete("Consider using Compute method instead.")]
         public Guid Create( in NamedGuidAlgorithm algorithm, in Guid @namespace, string name )
         {
             if ( name == null ) throw new ArgumentNullException( nameof( name ) );
@@ -82,6 +83,62 @@ namespace Identifiable.Factories
                 hash[8] |= 0b10000000;
                 hash[8] &= 0b10111111;
                 
+                return new Guid( hash );
+            }
+        }
+
+        /// <summary>
+        /// Computes and return a name-based GUID using the given algorithm as defined in https://tools.ietf.org/html/rfc4122#section-4.3.
+        /// </summary>
+        /// <param name="algorithm">Hash algorithm to use for generating the name. SHA-1 is recommended.</param>
+        /// <param name="namespace">Name space identifier.</param>
+        /// <param name="name">Name for which to create a GUID.</param>
+
+        public Guid Compute( in NamedGuidAlgorithm algorithm, in Guid @namespace, string name )
+        {
+            if ( name == null ) throw new ArgumentNullException( nameof( name ) );
+
+            // function to put guid bytes into network order
+            void correctEndianness( byte[] data )
+            {
+                /* from https://msdn.microsoft.com/en-us/library/windows/desktop/aa373931(v=vs.85).aspx
+                    typedef struct _GUID {
+                      DWORD Data1;
+                      WORD  Data2;
+                      WORD  Data3;
+                      BYTE  Data4[8]; // already big-endian
+                    } GUID;
+                */
+
+                if ( BitConverter.IsLittleEndian )
+                {
+                    Array.Reverse( data, 0, 4 );
+                    Array.Reverse( data, 4, 2 );
+                    Array.Reverse( data, 6, 2 );
+                }
+            }
+
+            var nameBytes = Encoding.UTF8.GetBytes( name );
+            var namespaceBytes = @namespace.ToByteArray();
+            correctEndianness( namespaceBytes );
+
+            using ( var hasher = algorithmFactory.Create( algorithm, out byte version ) )
+            {
+                hasher.TransformBlock( namespaceBytes, 0, namespaceBytes.Length, null, 0 );
+                hasher.TransformFinalBlock( nameBytes, 0, nameBytes.Length );
+                var hash = hasher.Hash;
+
+                Array.Resize( ref hash, 16 );
+                correctEndianness( hash );
+
+                // set version
+                hash[7] &= 0b00001111;
+                hash[7] |= version;
+
+                // set variant - turn on first bit, turn off second bit
+                hash[8] |= 0b10000000;
+                hash[8] &= 0b10111111;
+
                 return new Guid( hash );
             }
         }
--- test/Factories/NamedGuidFactoryTests.cs
+++ test/Factories/NamedGuidFactoryTests.cs
@@ -127,5 +127,96 @@ namespace Identifiable.Factories
                 Assert.NotEqual( expected, actual );
             }
         }
+
+        public class Compute : NamedGuidFactoryTests
+        {
+            NamedGuidAlgorithm algorithm;
+            Guid @namespace = Guid.NewGuid();
+            string name = string.Empty;
+
+            Guid invoke() => instance.Compute( algorithm, @namespace, name );
+
+            [Fact]
+            public void requires_name()
+            {
+                name = null;
+                Assert.Throws<ArgumentNullException>( nameof( name ), () => invoke() );
+            }
+
+            [Fact]
+            public void requires_valid_algorithm()
+            {
+                algorithm = default( NamedGuidAlgorithm );
+                Assert.Throws<NotImplementedException>( () => invoke() );
+            }
+
+            [Theory]
+            [InlineData( NamedGuidAlgorithm.MD5 )]
+            [InlineData( NamedGuidAlgorithm.SHA1 )]
+            public void returns_variant_1( NamedGuidAlgorithm algorithm )
+            {
+                name = $"{Guid.NewGuid()}";
+                this.algorithm = algorithm;
+                var actual = invoke();
+
+                // isolate the first two bits
+                var variant = actual.ToByteArray()[8] & 0b11000000;
+
+                // ensure the first bit is on, the second bit is off
+                Assert.Equal( 0b10000000, variant );
+            }
+
+            [Theory]
+            [InlineData( NamedGuidAlgorithm.MD5 )]
+            [InlineData( NamedGuidAlgorithm.SHA1 )]
+            public void returns_version_for_algorithm( NamedGuidAlgorithm algorithm )
+            {
+                name = $"{Guid.NewGuid()}";
+                this.algorithm = algorithm;
+                var actual = invoke();
+
+                // isolate the first nibble
+                var version = actual.ToByteArray()[7] & 0b11110000;
+
+                switch ( algorithm )
+                {
+                    case NamedGuidAlgorithm.MD5:
+                        Assert.Equal( StandardGuidVersion.UUIDv3, version );
+                        break;
+                    case NamedGuidAlgorithm.SHA1:
+                        Assert.Equal( StandardGuidVersion.UUIDv5, version );
+                        break;
+                    default:
+                        throw new InvalidOperationException();
+                }
+            }
+
+            public class DeterministicResults : TheoryData<NamedGuidAlgorithm, Guid, string, Guid>
+            {
+                public DeterministicResults()
+                {
+                    // example from https://tools.ietf.org/html/rfc4122#appendix-B
+                    // note the result comes from the errata: http://www.rfc-editor.org/errata_search.php?rfc=4122&eid=1352
+                    Add( NamedGuidAlgorithm.MD5, new Guid( "6ba7b810-9dad-11d1-80b4-00c04fd430c8" ), "www.widgets.com", new Guid( "3d813cbb-47fb-32ba-91df-831e1593ac29" ) );
+                }
+            }
+
+            [Theory]
+            [ClassData( typeof( DeterministicResults ) )]
+            public void returns_deterministic( NamedGuidAlgorithm algorithm, Guid @namespace, string name, Guid expected )
+            {
+                this.@namespace = @namespace;
+                this.algorithm = algorithm;
+                this.name = name;
+
+                var actual = invoke();
+                Assert.Equal( expected, actual );
+
+                // ensure changing the text yields a different result
+                this.name += "!";
+                actual = invoke();
+                Assert.NotEqual( expected, actual );
+            }
+        }
     }
 }
